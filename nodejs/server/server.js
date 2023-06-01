9a96f77e2527e7aa2ba64ae4e6e0267fe672757b
Your Name <you@example.com>
2023-06-01 20:56:31 +0300

    first

--- nodejs/server/server.js
+++ nodejs/server/server.js
@@ -1,20 +1,49 @@
 import http, { request } from "http";
 import fs from "fs";
+
+function getMimeType(path) {
+    let mimes = {
+        html: 'text/html',
+        jpeg: 'image/jpeg',
+        jpg: 'image/jpeg',
+        png: 'image/png',
+        svg: 'image/svg+xml',
+        json: 'application/json',
+        js: 'text/javascript',
+        css: 'text/css',
+        ico: 'image/x-icon',
+    };
+
+    let exts = Object.keys(mimes);
+    let extReg = new RegExp('\\.(' + exts.join('|') + ')$');
+
+    let ext = path.match(extReg)[1];
+
+    if (ext) {
+        return mimes[ext];
+    } else {
+        return 'text/plain';
+    }
+}
+
 http.createServer(async (request, response) => {
     let text;
-    if (request.url != '/favicon.ico') {
-        console.log(request.url)
-        switch (request.url) {
-            case "/page1":
-                text = await fs.promises.readFile('file1.html', 'utf8');
-                break;
-            case "/page2":
-                text = await fs.promises.readFile('file2.html', 'utf8');
+    let status;
+    let path = 'root' + request.url;
 
+    try {
+        if ((await fs.promises.stat(path)).isDirectory()) {
+            path += '/index.html';
         }
 
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(text);
-        response.end();
+        status = 200;
+        text = await fs.promises.readFile(path, 'utf8');
+    } catch (err) {
+        status = 404;
+        text = 'page not found';
     }
+
+    response.writeHead(status, { 'Content-Type': getMimeType(path) }); // изменение
+    response.write(text);
+    response.end();
 }).listen(3000);
