17258eb4e3d68a428365eb967d0e7ac5de8feadc
Your Name <you@example.com>
2023-06-02 20:34:44 +0300

    first

---
+++ lesson16/calculator2/js/cars_module.js
@@ -0,0 +1,5 @@
+export const cars = {
+    'Acura': [
+        "MDX", "RDX"
+    ]
+};
---
+++ lesson16/calculator2/js/script.js
@@ -0,0 +1,12 @@
+import { cars } from "./cars_module.js"
+
+console.log(cars)
+let brand_select = document.querySelector("#brand_select");
+let model_select = document.querySelector("#model_select");
+let year_select = document.querySelector("#year_select");
+let generation_select = document.querySelector("#generation_select");
+let modification_select = document.querySelector("#modification_select");
+let body_select = document.querySelector("#body_select");
+let equipment_select = document.querySelector("#equipment_select");
+let mileage_input = document.querySelector("#mileage_input");
+
