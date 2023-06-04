dcd5660c29cb1568e55a1fbc10bad0f8ca684184
Your Name <you@example.com>
2023-06-04 11:07:46 +0300

    first

--- lesson16/calculator2/js/script.js
+++ lesson16/calculator2/js/script.js
@@ -13,16 +13,16 @@ let mileage_input = document.querySelector("#mileage_input");
 let brand_list = Object.keys(cars);
 
 let option = document.createElement("option");
-option.setAttribute("value", "default");
+option.setAttribute("value", "caption");
 option.textContent = "Бренд";
 brand_select.appendChild(option);
 
 // let option = document.createElement("option");
 option = document.createElement("option");
-option.setAttribute("value", "default");
+option.setAttribute("value", "caption");
 option.textContent = "Модель";
 model_select.appendChild(option);
-model_select.disabled;
+model_select.setAttribute("disabled", "disabled");
 
 brand_list.forEach(elem => {
     let option = document.createElement("option");
@@ -30,16 +30,26 @@ brand_list.forEach(elem => {
     option.textContent = elem;
     brand_select.appendChild(option);
 });
-brand_select.addEventListener("change", function (event) {
 
-    if (this.value) {
+brand_select.addEventListener("change", function (event) {
+    console.log(this.value)
+    let opt_list = model_select.querySelectorAll("option");
+    console.log(opt_list)
+    opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+
+    if (cars[this.value]) {
         cars[this.value].forEach(elem => {
             let option = document.createElement("option");
             option.setAttribute("value", elem);
             option.textContent = elem;
-
-            // Добавляем option в select
             model_select.appendChild(option);
         })
+        model_select.removeAttribute("disabled");
+    } else {
+        model_select.setAttribute("disabled", "disabled");
     }
 })
