bb1b0912e7f90cdd2984b3cdc2fdbacc49a6c1c7
Your Name <you@example.com>
2023-06-05 14:54:03 +0300

    first

--- lesson16/calculator2/js/cars_module.js
+++ lesson16/calculator2/js/cars_module.js
@@ -10,3 +10,8 @@ export const cars = {
         "DB11", "DB9"
     ]
 };
+export const years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'];
+export const generation = ["Первое", "Второе", "Третье"];
+export const modification = ["1", "2", "3"];
+export const body = ["Седан", "Минивен"];
+export const equipment = ["Дешевое", "Крутое"]
--- lesson16/calculator2/js/script.js
+++ lesson16/calculator2/js/script.js
@@ -1,6 +1,6 @@
-import { cars } from "./cars_module.js"
+import { cars, years, generation, modification, body, equipment } from "./cars_module.js"
 
-console.log(cars)
+// console.log(cars)
 let brand_select = document.querySelector("#brand_select");
 let model_select = document.querySelector("#model_select");
 let year_select = document.querySelector("#year_select");
@@ -9,6 +9,8 @@ let modification_select = document.querySelector("#modification_select");
 let body_select = document.querySelector("#body_select");
 let equipment_select = document.querySelector("#equipment_select");
 let mileage_input = document.querySelector("#mileage_input");
+let calculate = document.querySelector(".calculate");
+
 
 let brand_list = Object.keys(cars);
 
@@ -68,10 +70,42 @@ brand_list.forEach(elem => {
 });
 
 brand_select.addEventListener("change", function (event) {
-    console.log(this.value)
     let opt_list = model_select.querySelectorAll("option");
-    console.log(opt_list)
+    // console.log(opt_list)
     opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+
+    let year_opt_list = year_select.querySelectorAll("option");
+    // console.log(year_opt_list)
+    year_opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+    let generation_opt_list = generation_select.querySelectorAll("option");
+    generation_opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+
+    let modification_opt_list = modification_select.querySelectorAll("option");
+    modification_opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+    let body_opt_list = body_select.querySelectorAll("option");
+    body_opt_list.forEach(elem => {
+        if (elem.value !== "caption") {
+            elem.remove()
+        }
+    })
+    let equipment_opt_list = equipment_select.querySelectorAll("option");
+    equipment_opt_list.forEach(elem => {
         if (elem.value !== "caption") {
             elem.remove()
         }
@@ -85,7 +119,141 @@ brand_select.addEventListener("change", function (event) {
             model_select.appendChild(option);
         })
         model_select.removeAttribute("disabled");
+
+        years.forEach(year => {
+            let option = document.createElement("option");
+            option.setAttribute("value", year);
+            option.textContent = year;
+            year_select.appendChild(option);
+        })
+        year_select.removeAttribute("disabled");
+
+        generation.forEach(gen => {
+            // console.log(gen)
+            let option = document.createElement("option");
+            option.setAttribute("value", gen);
+            option.textContent = gen;
+            generation_select.appendChild(option);
+            // console.log(generation_select)
+        })
+        generation_select.removeAttribute("disabled");
+
+        modification.forEach(gen => {
+            // console.log(gen)
+            let option = document.createElement("option");
+            option.setAttribute("value", gen);
+            option.textContent = gen;
+            modification_select.appendChild(option);
+        })
+        modification_select.removeAttribute("disabled");
+
+        body.forEach(body => {
+            let option = document.createElement("option");
+            option.setAttribute("value", body);
+            option.textContent = body;
+            body_select.appendChild(option);
+        })
+        body_select.removeAttribute("disabled");
+
+        equipment.forEach(equipment => {
+            let option = document.createElement("option");
+            option.setAttribute("value", equipment);
+            option.textContent = equipment;
+            equipment_select.appendChild(option);
+        })
+        equipment_select.removeAttribute("disabled");
+
     } else {
         model_select.setAttribute("disabled", "disabled");
+        year_select.setAttribute("disabled", "disabled");
+        generation_select.setAttribute("disabled", "disabled");
+        body_select.setAttribute("disabled", "disabled");
+        modification_select.setAttribute("disabled", "disabled");
+        equipment_select.setAttribute("disabled", "disabled");
+    }
+})
+
+calculate.addEventListener("click", function (event) {
+    let brand_k, model_k, year_k, generation_k, modification_k, body_k, equipment_k, mileage_k;
+    let price;
+    switch (brand_select.value) {
+        case "Acura": brand_k = 900000;
+            break;
+        case "Alfa Romeo": brand_k = 500000;
+            break;
+        default: brand_k = 0;
+    }
+    switch (model_select.value) {
+        case "MDX": model_k = 0.9;
+            break;
+        case "RDX": model_k = 0.8;
+            break;
+        case "Brera": model_k = 0.7;
+            break;
+        case "Mito": model_k = 0.75;
+            break;
+        case "DB11": model_k = 0.85;
+            break;
+        case "DB9": model_k = 0.95;
+            break;
+        default: model_k = 1;
+    }
+    if (Number(year_select.value)) {
+        year_k = Number(year_select.value) / 2023
+        console.log(year_k)
+    } else {
+        year_k = 1;
+    }
+    switch (generation_select.value) {
+        case "Первое": generation_k = 0.8;
+            break;
+        case "Второе": generation_k = 0.9;
+            break;
+        case "Третье": generation_k = 1;
+            break;
+        default: generation_k = 1;
+
+
+    }
+
+    switch (modification_select.value) {
+        case "1": modification_k = 0.8;
+            break;
+        case "2": modification_k = 0.9;
+            break;
+        case "3": modification_k = 1;
+            break;
+        default: modification_k = 1;
+    }
+
+    switch (body_select.value) {
+        case "Седан": body_k = 0.8;
+            break;
+        case "Минивен": body_k = 1;
+            break;
+        default: body_k = 1;
+    }
+
+    switch (equipment_select.value) {
+        case "Седан": equipment_k = 0.8;
+            break;
+        case "Минивен": equipment_k = 1;
+            break;
+        default: equipment_k = 1;
+    }
+    let mile_age = Number(mileage_input.value);
+    if (mile_age < 100000) {
+        mileage_k = 1;
+    } else if (mile_age >= 100000 && mile_age <= 500000) {
+        mileage_k = 0.8;
+    } else {
+        mileage_k = 0.6;
+    }
+
+    price = Math.floor(brand_k * model_k * year_k * generation_k * modification_k * body_k * equipment_k * mileage_k);
+    if (price) {
+        alert('Ориентировочная стоимость автомобиля\n' + price + '  рублей');
+    } else {
+        alert('Выберите марку авто');
     }
 })
