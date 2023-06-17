2eea9e5531e7b1c870c294a304651f9883bff57d
androsovichslava <androsovichslava@gmail.com>
2023-06-17 18:01:12 +0300

    first

--- express/index.js
+++ express/index.js
@@ -1,25 +1,71 @@
 import express from 'express';
+import __dirname from './__dirname.js';
+
+console.log(__dirname);
 let app = express();
 app.get('/', function (req, res) {
-    console.log(req.path);
-    console.log(req.url);
-    console.log(req.originalUrl);
-    console.log(req.query);
-    console.log(req.protocol);
-    console.log(req.secure);
-    console.log(req.acceptsLanguages());
-    console.log(req.ip);
     res.send("<h1>HELLO</h1>");
 });
-app.get('/1/', function (req, res) {
-    res.send("<h1>HELLO page1</h1>");
+// app.get('/page/1/', function (req, res) {
+//     res.sendFile(__dirname + '/pages/1.html');
+// });
+
+// app.get('/page/2/', function (req, res) {
+//     res.sendFile(__dirname + '/pages/2.html');
+// });
+// app.get('/page/3/', function (req, res) {
+//     res.sendFile(__dirname + '/pages/3.html');
+// });
+app.get('/page/:num/:num2/:num3/', function (req, res) {
+    res.send(`<h1>Страница: ${req.params.num} Сумма чисел: ${Number(req.params.num2) + Number(req.params.num3)}</h1>`);
 });
-app.get('/2/', function (req, res) {
-    res.send("<h1>HELLO page2</h1>");
+app.get('/menu/', function (req, res) {
+    let menu = ['Main', 'content', 'About us'];
+
+    res.send('<ul>' + menu.map(elem => `<li>${elem}</li>`).join('') + '</ul>');
 });
-app.get('/3/', function (req, res) {
-    res.send("<h1>HELLO page3</h1>");
+
+app.get('/prods/', function (req, res) {
+    let prods = [
+        {
+            name: 'prod1',
+            cost: 1000,
+        },
+        {
+            name: 'prod2',
+            cost: 2000,
+        },
+        {
+            name: 'prod3',
+            cost: 3000,
+        },
+    ];
+
+    res.send('<ul>' + prods.map(elem => `<li>${elem.name}  ${elem.cost}</li>`).join('') + '</ul>');
 });
+app.get('/employees/', function (req, res) {
+    let employees = [
+        {
+            surname: 'surname1',
+            name: 'user1',
+            salary: 1000,
+        },
+        {
+            surname: 'surname2',
+            name: 'user2',
+            salary: 2000,
+        },
+        {
+            surname: 'surname3',
+            name: 'user3',
+            salary: 3000,
+        },
+    ];
+    res.send('<table>' + employees.map(elem => `<tr><td>${elem.name}</td> <td> ${elem.surname}</td><td> ${elem.salary}</td></tr>`).join('') + '</table>');
+});
+
+
+app.use(express.static(__dirname + '/public/'))
 app.use(function (req, res) {
     res.status(404).send('not found');
 });
